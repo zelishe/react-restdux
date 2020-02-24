3f55c3c633ebffead5792e7703f44a232c7912cd
Sergei Zelentsov <sergey.zelentsoff@gmail.com>
2020-02-24 17:08:44 +0200

    Error response fixes

--- src/apiCallFunctions.ts
+++ src/apiCallFunctions.ts
@@ -13,13 +13,14 @@ import * as qs from 'qs';
 
 const collectionParseErrorMessage = '[Restdux][EntityCrudService] Could not parse http response. Define or update parseCollectionHttpResponse in entityStoreConfig, please';
 const entityParseErrorMessage     = '[Restdux][EntityCrudService] Could not parse http response. Define or update parseEntityHttpResponse in entityStoreConfig, please';
+const noAxiosProvidedErrorMessage = 'No axios instance was provided in entityStoreConfig';
 
 const processEntityHttpResponse = <T>(entityStoreConfig: EntityStoreConfig, initialHttpResponse: any, params: any): T => {
 
   const entity = entityStoreConfig.parseEntityHttpResponse(initialHttpResponse, params);
 
   if (!entity) {
-    onError(entityParseErrorMessage);
+    throw new Error(entityParseErrorMessage);
   }
 
   return entity;
@@ -34,13 +35,9 @@ const getQueryString = (query: any = null) => {
   return ((queryString !== '') ? '?' : '') + queryString;
 };
 
-const onError = (error: any) => {
-  throw new Error(error);
-};
-
 const getAxiosInstance = (entityStoreConfig: EntityStoreConfig): AxiosInstance => {
   if (!entityStoreConfig.axios) {
-    throw new Error('No axios instance was provided in entityStoreConfig');
+    throw new Error(noAxiosProvidedErrorMessage);
   }
 
   return entityStoreConfig.axios;
@@ -48,61 +45,39 @@ const getAxiosInstance = (entityStoreConfig: EntityStoreConfig): AxiosInstance =
 
 export const findAllApiCall = async <T>(entityStoreConfig: EntityStoreConfig, entityName: string, filter: any = null) => {
 
-  try {
+  const httpResponse = await getAxiosInstance(entityStoreConfig).get(`${getApiEndpoint(entityStoreConfig, entityName)}${getQueryString(filter)}`);
+  const entityStorePage: EntityStorePage<T> = entityStoreConfig.parseCollectionHttpResponse(httpResponse, { filter });
 
-    const httpResponse = await getAxiosInstance(entityStoreConfig).get(`${getApiEndpoint(entityStoreConfig, entityName)}${getQueryString(filter)}`);
-    const entityStorePage: EntityStorePage<T> = entityStoreConfig.parseCollectionHttpResponse(httpResponse, { filter });
-
-    if (!entityStorePage || !entityStorePage.entities || !entityStorePage.totalEntities) {
-      throw new Error(collectionParseErrorMessage);
-    }
-
-    return entityStorePage;
-  } catch (error) {
-    return onError(error);
+  if (!entityStorePage || !entityStorePage.entities || !entityStorePage.totalEntities) {
+    throw new Error(collectionParseErrorMessage);
   }
+
+  return entityStorePage;
 
 };
 
 export const findByKeyApiCall = async <T>(entityStoreConfig: EntityStoreConfig, entityName: string, key: number | string) => {
-
-  try {
-    const httpResponse = await getAxiosInstance(entityStoreConfig).get(`${getApiEndpoint(entityStoreConfig, entityName)}/${key}`);
-    return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { key });
-  } catch (error) {
-    return onError(error);
-  }
-
+  const httpResponse = await getAxiosInstance(entityStoreConfig).get(`${getApiEndpoint(entityStoreConfig, entityName)}/${key}`);
+  return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { key });
 };
 
 export const saveApiCall = async <T extends { [key: string]: any }>(entityStoreConfig: EntityStoreConfig, entityName: string, entity: T) => {
 
   let httpResponse;
 
-  try {
-    if (entity['id']) {
-      httpResponse = await getAxiosInstance(entityStoreConfig).put(`${getApiEndpoint(entityStoreConfig, entityName)}/${entity['id']}`, entity);
-    } else {
-      httpResponse = await getAxiosInstance(entityStoreConfig).post(`${getApiEndpoint(entityStoreConfig, entityName)}`, entity);
-    }
-
-    return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { entity });
-
-  } catch (error) {
-    return onError(error);
+  if (entity['id']) {
+    httpResponse = await getAxiosInstance(entityStoreConfig).put(`${getApiEndpoint(entityStoreConfig, entityName)}/${entity['id']}`, entity);
+  } else {
+    httpResponse = await getAxiosInstance(entityStoreConfig).post(`${getApiEndpoint(entityStoreConfig, entityName)}`, entity);
   }
+
+  return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { entity });
 
 };
 
 export const deleteByKeyApiCall = async <T extends { [key: string]: any }>(entityStoreConfig: EntityStoreConfig, entityName: string, key: any) => {
-
-  try {
-    const httpResponse = await getAxiosInstance(entityStoreConfig).delete(`${getApiEndpoint(entityStoreConfig, entityName)}/${key}`);
-    return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { key });
-  } catch (err) {
-    return onError(err);
-  }
-
+  const httpResponse = await getAxiosInstance(entityStoreConfig).delete(`${getApiEndpoint(entityStoreConfig, entityName)}/${key}`);
+  return processEntityHttpResponse<T>(entityStoreConfig, httpResponse, { key });
 };
 
 export const constructApiCall = async (
